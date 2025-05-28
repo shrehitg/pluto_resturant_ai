603649b63aa9859a88e35de7404828a132b6f93c
Shrehit Goel <shrehit@Shrehits-MacBook-Air.local>
2025-05-27 18:13:11 -0700

    Allow obvious common knowledge questions while filtering complex unrelated topics

--- main.py
+++ main.py
@@ -147,7 +147,8 @@ SYSTEM_MESSAGE = (
     
     "HANDLING OFF-TOPIC QUESTIONS:\n"
     "Answer questions that are remotely related to the restaurant experience (nearby parking, attractions, neighborhood info, "
-    "directions to Shizen, etc.). For completely unrelated topics (weather, sports, politics, general trivia), "
+    "directions to Shizen, etc.) and obvious common knowledge questions anyone would know (color of sky, basic facts). "
+    "For complex unrelated topics (sports scores, politics, technical support, personal advice), "
     "politely respond: 'I don't know about that, but I can help with Shizen questions - menu, reservations, or orders.'\n\n"
     
     "REALISTIC RESERVATION HANDLING:\n"
